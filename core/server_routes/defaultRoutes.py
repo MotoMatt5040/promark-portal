8c7c5d628b21d2bc70642b23879b662daa134b0d
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-09 16:46:30 -0500

    Updated default routes so image can be built with minimal code

---
+++ core/server_routes/defaultRoutes.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from config import Config
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from utils.database.datapuller import DataPuller
+
+app = Flask(__name__)
+
+# TODO This was added to test login and permissions (user authentication)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+# TODO This was added to test login and permissions (user authentication)
+
+data_puller = DataPuller()
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    """App home page"""
+    # return render_template('')
+    # return render_template('home.html')
+    # print("connection found")
+    return {"test Data": "1"}
