a5f029454ccddc406da9f6379c0366cc8c8e23bb
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-16 16:37:52 -0500

    login stylesheet added

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,17 +2,28 @@ import Form from 'react-bootstrap/Form';
 
 function FormGroupExample() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-    </Form>
+    <div className="login-div" style={stylesheet}>
+      <Form>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form>
+    </div>
+
   );
 }
 
 export default FormGroupExample;
+
+const stylesheet = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  alignContent: "center",
+  height: "100vh"
+}
