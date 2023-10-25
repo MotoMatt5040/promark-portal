139da2747d593b18ad1a2f274f0be8a15fe8eb39
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-25 12:08:47 -0500

    Commented out table creating line, not needed now as the tables have been created. added dataprocessing import and route

--- server/defaults/core/server_routes/defaultRoutes.py
+++ server/defaults/core/server_routes/defaultRoutes.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, make_response
 from flask_bcrypt import Bcrypt
 from flask_session import Session
 from flask_cors import CORS
+from ..data_processing.reader import Reader
+# from flask-redis import Redis
+from pathlib import Path
 
 from .config import ApplicationConfig
 from ..auth.models import db, User
@@ -9,15 +12,15 @@ from ..auth.models import db, User
 # from defaults.utils.database.datapuller import DataPuller
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True, origins=['http://localhost:3001'])
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
 server_session = Session(app)
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 # data_puller = DataPuller()
 
@@ -38,14 +41,22 @@ def testthingy():
     print("IM WORKING")
     return {"test Data": "1"}
 
-@app.route("/@me")
+@app.route("/@me", methods=['GET'])
 def get_current_user():
     user_id = session.get("user_id")
+    print('\n')
+    print(session)
+    print('\n')
 
     if not user_id:
         return jsonify({"error": "Unauthorized"}), 401
 
     user = User.query.filter_by(id=user_id).first()
+
+    print(jsonify({
+        "id": user.id,
+        "email": user.email
+    }))
 
     return jsonify({
         "id": user.id,
@@ -76,13 +87,8 @@ def register():
 
 @app.route('/login', methods=['POST'])
 def login():
-    # if request.method == "OPTIONS":
-    #     # print(request.json)
-    #     return {'options': 'some stuff'}
-    print(request.json)
     email = request.json['email']
     password = request.json['password']
-
 
     user = User.query.filter_by(email=email).first()
     if user is None:
@@ -93,8 +99,28 @@ def login():
 
     session["user_id"] = user.id
 
+    print('\n')
+    print(session)
+    print('\n')
 
     return jsonify({
         "id": user.id,
         "email": user.email
     })
+
+
+@app.route('/data_processing', methods=['POST', 'GET'])
+def data_processing():
+    survey_id = request.json['survey_id']
+    project_id = request.json['project_id']
+
+    origin_path = Path(rf"i:\PROJ\{project_id}")
+    database_path = Path(rf"\DATABASE\{project_id}")
+
+    Reader(project_id).setUrl(survey_id)
+
+    return Path(rf"{origin_path}\UNCLE\{project_id} tables"), \
+        Path(f"{origin_path}{database_path} layout.xlsx"), \
+        Path(f"{origin_path}{database_path} xfile.xlsx")
+
+
