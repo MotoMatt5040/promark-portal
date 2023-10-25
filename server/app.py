1aa8a502eb2176056f7513efab6e007e1d03e6e2
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-25 12:07:06 -0500

    key change

--- server/app.py
+++ server/app.py
@@ -2,5 +2,6 @@ from defaults.core.server_routes import defaultRoutes
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    defaultRoutes.app.run(ssl_context=('c38827a1bd357111.pem', 'promarkresearch.com.key'))  # this is required to run the proxy
+    # defaultRoutes.app.run(debug=True)
+    # defaultRoutes.app.run(ssl_context=('c38827a1bd357111.pem', 'promarkresearch.com.key'))  # this is required to run the proxy
+    defaultRoutes.app.run(ssl_context=('b27662e91187c7e3.crt', 'promarkresearch.com.key'))
