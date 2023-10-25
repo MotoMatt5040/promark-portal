1774b1c75b7eb49b7c33b1e87cff272d9b139eab
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-25 12:09:12 -0500

    removed texting platform, this will likely be phased out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,15 +12,16 @@ const Header = (props) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="https://https://dashapp.promarkresearch.com/">Dashboard</Nav.Link>
             <Nav.Link href="production_report">Production Report</Nav.Link>
-            <NavDropdown title="Texting Platform" id="basic-nav-dropdown">
-              <NavDropdown.Item href="texting_platform/sample_upload">Sample Upload</NavDropdown.Item>
-              <NavDropdown.Item href="#texting_platform/next_item">Next Item</NavDropdown.Item>
-              <NavDropdown.Item href="#texting_platform/potential_third_item">Potential Third Item</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#texting_platoform/send_message">Send Messages</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="data_processing">Data Processing</Nav.Link>
+            {/*<NavDropdown title="Texting Platform" id="basic-nav-dropdown">*/}
+            {/*  <NavDropdown.Item href="texting_platform/sample_upload">Sample Upload</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Item href="#texting_platform/next_item">Next Item</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Item href="#texting_platform/potential_third_item">Potential Third Item</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Divider />*/}
+            {/*  <NavDropdown.Item href="#texting_platoform/send_message">Send Messages</NavDropdown.Item>*/}
+            {/*</NavDropdown>*/}
           </Nav>
         </Navbar.Collapse>
       </Container>
