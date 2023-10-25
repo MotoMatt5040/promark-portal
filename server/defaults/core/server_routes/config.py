3238be0a7dc17da32c96dbbed064753a486c67e2
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-25 12:07:25 -0500

    added cookie management

--- server/defaults/core/server_routes/config.py
+++ server/defaults/core/server_routes/config.py
@@ -19,15 +19,17 @@ SQL_CONNECTION = 'DRIVER={};SERVER={};DATABASE={};UID={};PWD={}'.format(
     dbai.get_password()
 )
 del dbai
-dbcon = f'mssql+pyodbc:///?odbc_connect={SQL_CONNECTION}'
+uri = f'mssql+pyodbc:///?odbc_connect={SQL_CONNECTION}'
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = dbcon
+    SQLALCHEMY_DATABASE_URI = uri
 
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True
