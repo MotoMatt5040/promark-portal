faf7299602d35dec40ab1ba5016f1caaf0eb7945
MotoMatt5040 <motomatt5040@gmail.com>
2023-10-27 16:02:50 -0500

    updated accordion

--- client/src/pages/data-processing/DataProcessing.js
+++ client/src/pages/data-processing/DataProcessing.js
@@ -3,6 +3,8 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 import axios from "../../api/axios";
+import surveyDropdown from "./assets/survey_dropdown.PNG";
+import surveyIdImage from './assets/survey_id.png';
 
 
 const DATA_PROCESSING_URL = '/data_processing';
@@ -38,7 +40,7 @@ function SampleUpload() {
       );
 
       if (response.status === 200) {
-        // window.location.href="/home"
+        window.location.href="#"
         console.log('Request sent for data processing')
       }
         console.log(JSON.stringify(response));
@@ -67,7 +69,7 @@ function SampleUpload() {
               <Form.Group className="mb-3" controlId="formGroupSruveryId">
                 <Form.Label>Survey ID</Form.Label>
                 <Form.Control
-                  type="email"
+                  type="text"
                   autoComplete="off"
                   onChange={(e) => setSurveyID(e.target.value)}
                   placeholder="Survey ID"
@@ -81,7 +83,7 @@ function SampleUpload() {
               <Form.Group className="mb-3" controlId="formGroupProjectId">
                 <Form.Label>Project ID</Form.Label>
                 <Form.Control
-                  type="email"
+                  type="text"
                   autoComplete="off"
                   onChange={(e) => setProjectID(e.target.value)}
                   placeholder="Project ID"
@@ -103,7 +105,7 @@ function SampleUpload() {
           <Accordion.Item eventKey="0">
             <Accordion.Header>Step 1: Create order.csv</Accordion.Header>
               <Accordion.Body>
-                <p>Note: This only works if there is a project in Acuity.</p>
+                <p><i>Note: This only works if there is a project in Acuity.</i></p>
                 <br />
                 <br />
                 <p>
@@ -119,7 +121,7 @@ function SampleUpload() {
                   &ensp; Check the following: <i>Note: these are located at the bottom</i><br />
                   &emsp; <b>Strip HTML from labels</b> <br />
                   &emsp; <b>Merge multiple mention responses and open-ends</b> <br />
-                  Once finished, click <b>[Export]</b> <i>in the top right</i>
+                  Once finished, click <b>[Export]</b> in the top right.
                 </p>
               </Accordion.Body>
           </Accordion.Item>
@@ -127,15 +129,51 @@ function SampleUpload() {
             <Accordion.Header>Step 2: Download order.csv</Accordion.Header>
               <Accordion.Body>
                 <p>
-                  Save <b>order.csv</b> in <b>DATABASE</b> folder for the project you are working on.<br />
-                  It is important that this file is named <b>order.csv</b> exactly as specified.
+                  Create a folder called <b>[DATAPROC]</b> inside the project folder. <br/>
+                  Create a folder called <b>[DATABASE]</b> and <b>[UNCLE]</b> inside the <b>[DATAPROC]</b> folder. <br/>
+                  Create a folder called <b>[OUTPUT]</b> inside the <b>[UNCLE]</b> folder. <br/>
+                  Save <b>order.csv</b> in the <b>[DATABASE]</b> folder for the project you are working on.<br />
+                  It is important that this file is named <b>order.csv</b> exactly as specified. <br/>
+                  The project directory should have the following format. <br/>
+                  &ensp; <b>&lt;project id&gt;</b> <br/>
+                  &emsp;&emsp;&emsp;.<br/>
+                  &emsp;&emsp;&emsp; <b>[DATAPROC]</b> <br/>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;.<br/>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>[DATABASE]</b><br/>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;. . .<br/>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>[UNCLE]</b><br/>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;.<br/>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>[OUTPUT]</b><br/>
                 </p>
               </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
-            <Accordion.Header>Step 3: </Accordion.Header>
+            <Accordion.Header>Step 3:  Enter Survey ID and Project ID</Accordion.Header>
             <Accordion.Body>
-              Uploaded data for sample 2
+              <p>
+                Finding Survey ID: <i>Note: This can only be done while in the Surveys section.</i><br />
+                &ensp; Click the dropdown in the top left corner <br />
+                &emsp; <img src={surveyDropdown} alt="Survey Dropdown"/>
+                &emsp; <img src={surveyIdImage} alt="Survey ID"/> <br />
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Step 4: Press Run</Accordion.Header>
+            <Accordion.Body>
+              <p>After filling out the fields from the previous step you can run the program.</p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>Step 5: Verifiy the created files</Accordion.Header>
+            <Accordion.Body>
+              <p>After filling out the fields from the previous step you can run the program.</p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="5">
+            <Accordion.Header>Step 6:</Accordion.Header>
+            <Accordion.Body>
+              <p></p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
