62f97d727648a2b85819c16663e3635a5cabea63
MotoMatt5040 <motomatt5040@gmail.com>
2023-11-02 17:40:28 -0500

    added waitress

--- server/app.py
+++ server/app.py
@@ -1,5 +1,13 @@
 from defaults.core.server_routes import defaultRoutes
+from waitress import serve
 
 if __name__ == '__main__':
-    defaultRoutes.app.run(debug=True)
+    print("http://localhost:5000/")
+    serve(
+        defaultRoutes.app,
+        host='0.0.0.0',
+        port=5000,
+        threads=10,
+        url_scheme="http"
+    )
     # defaultRoutes.app.run(ssl_context=('b27662e91187c7e3.crt', 'promarkresearch.com.key'))
