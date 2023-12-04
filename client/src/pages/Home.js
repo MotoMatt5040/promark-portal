1cda6e1fa8f1ecd4f72dccee2a4dcd6bbd2a2fed
MotoMatt5040 <motomatt5040@gmail.com>
2023-12-04 14:02:11 -0600

    Adjust Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,11 +20,13 @@ function Home() {
       <main>
         <div className="col-md-6">
           <div className="h-100 p-5 bg-light border rounded-3">
-            <h2>Add borders</h2>
-            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure
-              to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's
-              content for equal-height.</p>
-            <button className="btn btn-outline-secondary" type="button">Example button</button>
+            <h2>Promark Portal</h2>
+            <p>
+              Currently available features:<br/>
+              &emsp;Dashboard<br/>
+              &emsp;Data Processing
+            </p>
+            {/*<button className="btn btn-outline-secondary" type="button">Example button</button>*/}
           </div>
         </div>
 
