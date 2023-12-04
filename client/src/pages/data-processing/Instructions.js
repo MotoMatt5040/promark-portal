76eb6d0d4495418c18505c3d18a3fa63d47ea12e
MotoMatt5040 <motomatt5040@gmail.com>
2023-12-04 16:43:52 -0600

    Post download instructions with tooltips set up

--- client/src/pages/data-processing/Instructions.js
+++ client/src/pages/data-processing/Instructions.js
@@ -4,19 +4,20 @@ import surveyIdImage from './assets/survey_id.png';
 import jt from './assets/j-t.png';
 import TableBody from './tableBody.js'
 import projTitle from "./assets/project-title-caseid.png"
+import Tooltip from "@mui/material/Tooltip";
+import React from 'react';
 
 function Instructions() {
-
+  let cleanup_instructions = "The tables output by the program aren't always in the proper finalized format for output.";
   return (
     <Accordion>
       <Accordion.Item eventKey="0">
         <Accordion.Header>Step 1: Create order.csv</Accordion.Header>
           <Accordion.Body>
-            <p><i>Note: This only works if there is a project in Acuity.</i></p>
-            <br />
-            <br />
             <p>
-              Navigate to the survey in acuity <b>-> Analyze -> Export Responses</b> <br />
+              <i>Note: This only works if there is a project in Acuity.</i><br/>
+              <br/>
+              Navigate to the survey in Acuity <b>-> Analyze -> Export Responses</b> <br />
               &ensp; Press: <b>[New] -> CSV</b> <br />
               &ensp; Name: <b>order</b> <br />
               &ensp; Destination File Name: <b>order</b> <br />
@@ -26,10 +27,12 @@ function Instructions() {
               &ensp;&emsp; Press: <b>[Select Variables]</b> <br />
               &ensp; Select all variables to include <br />
               &ensp; Check the following: <i>Note: these are located at the bottom</i><br />
+              &emsp; <b>Include header</b> <br />
               &emsp; <b>Strip HTML from labels</b> <br />
               &emsp; <b>Remove curly brackets and spaces of system variables</b><br/>
               &emsp; <b>Merge multiple mention responses and open-ends</b> <br />
-              Once finished, click <b>[Export]</b> in the top right.
+              Once finished, click <b>[Export]</b> in the top right.<br/>
+              No further instructions for this task. Please proceed to step 2.
             </p>
           </Accordion.Body>
       </Accordion.Item>
@@ -38,23 +41,19 @@ function Instructions() {
           <Accordion.Body>
             <p>
               Create a folder called <b>[DATAPROC]</b> inside the project folder. <br/>
-              Create a folder called <b>[DATABASE]</b> and <b>[UNCLE]</b> inside the <b>[DATAPROC]</b> folder. <br/>
+              Create folders called <b>[DATABASE]</b> and <b>[UNCLE]</b> inside the <b>[DATAPROC]</b> folder. <br/>
               Create a folder called <b>[OUTPUT]</b> inside the <b>[UNCLE]</b> folder. <br/>
               The project directories should have the following format. <br/>
-              &ensp; <b>&lt;project id&gt;</b> <br/>
-              &emsp;&emsp;&emsp;|<br/>
-              &emsp;&emsp;&emsp; <b>[DATAPROC]</b> <br/>
-              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;|<br/>
-              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>[DATABASE]</b><br/>
-              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;. . .<br/>
-              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>[UNCLE]</b><br/>
-              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;|<br/>
-              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>[OUTPUT]</b><br/>
+              &ensp; <b>i:PROJ\&lt;prc project number&gt;</b> <br/>
+              &emsp;&emsp;&emsp;<b>&gt;[DATAPROC]</b> <br/>
+              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>&gt;[DATABASE]</b><br/>
+              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>&gt;[UNCLE]</b><br/>
+              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>&gt;[OUTPUT]</b><br/>
             </p>
           </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2">
-        <Accordion.Header>Step 3:  Enter Survey ID and Project ID</Accordion.Header>
+        <Accordion.Header>Step 3:  Enter Survey ID and Project Number</Accordion.Header>
         <Accordion.Body>
           <p>
             Finding Survey ID: <i>Note: This can only be done while in the Surveys section.</i><br />
@@ -65,34 +64,35 @@ function Instructions() {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="3">
-        <Accordion.Header>Step 4: Press Layout</Accordion.Header>
+        <Accordion.Header>Step 4: Click Layout</Accordion.Header>
         <Accordion.Body>
           <p>Uncheck the boxes for tables that will be skipped. These will include fills, computations, OS, OE, etc...</p>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="4">
-        <Accordion.Header>Step 5: Press Download</Accordion.Header>
+        <Accordion.Header>Step 5: Click Download</Accordion.Header>
         <Accordion.Body>
           <p>
             Download the zip file and copy the files into the following directories: <br/>
             &ensp;<b>[DATABASE]</b><br/>
             &emsp;<b>layout.csv</b><br/>
-            &emsp;<b>x-file.csv</b><br/>
+            &emsp;<b>xfile.csv</b><br/>
             &ensp;<b>[UNCLE]</b><br/>
             &emsp;<b>tables.txt</b><br/>
           </p>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="5">
-        <Accordion.Header>Step 6: create the project dat file</Accordion.Header>
+        <Accordion.Header>Step 6: Create the project<i>&nbsp;dat&nbsp;</i>file</Accordion.Header>
         <Accordion.Body>
           <p>
-            Navigate back to the location where you created the <b>order.csv</b> file in Acuity.<br/>
-            Click the dropdown menu on the <b>order</b> extraction task. <br/>
+            Navigate to the survey in Acuity <b>-> Analyze -> Export Responses</b> (the location where you created the <b>order</b> extraction task.)<br/>
+            Click the hamburger menu on the <b>order</b> extraction task. <br/>
             Click <b>copy</b><br/>
-            Change the name and destination file name to <b>&lt;project id&gt;dat</b><br/>
+            Change the name and destination file name to <b>&lt;project number&gt;dat</b><br/>
             Uncheck <b>Merge multiple mention responses and open-ends</b><br/>
-            Click save and run the extraction task. <br/>
+            Click <b>[Export]</b> to save the task.<br/>
+            Alow the task to run until the status indicates <b>Completed</b>.<br/>
             Download the file to your projects <b>[DATABASE]</b> folder. <br/>
           </p>
         </Accordion.Body>
@@ -101,18 +101,20 @@ function Instructions() {
         <Accordion.Header>Step 7: Run the file through USort</Accordion.Header>
         <Accordion.Body>
           <p>
-            Open <b>x-file.csv</b> and <b>&lt;projectid&gt;dat.csv</b>.<br/>
-            Copy the <b>2nd</b> row of the x-file <i>the row of x's</i> and insert it under the <b>header</b> row of the dat file.<br/>
-            &ensp;<i>Note: This is done so that the width of each data column is properly set for USort. It is important that you <b>DO NOT SKIP</b> this step.</i><br/>
+            Open <b>xfile.csv</b> and <b>&lt;projectid&gt;dat.csv</b>.<br/>
+            Copy the <b>2nd</b> row of the xfile <i>(the row of x's)</i> and insert it under the <b>header</b> row of the dat file.<br/>
+            &ensp;<i>Note: This is done so that the width of each data column is properly set for USort. It is important that you <b>***DO NOT SKIP***</b> this step.</i><br/>
             Save and close the dat file.<br/>
             Open <b>USort</b><br/>
             Check the following: <br/>
             &ensp;<b>Expand Delimeters</b><br/>
+            &emsp;<i>Then...</i><br/>
             &ensp;<b>Header</b><br/>
-            In the <b>middle</b> section, click <b>[Browse]</b> and navigate to your dat file<br/>
-            On the right side click <b>[Report File]</b> and save it as <b>expand.txt</b> in the <b>[DATABASE</b> folder. <br/>
-            On the bottom left click <b>[Browse]</b> and navigated to the <b>[UNCLE]</b> folder. <br/>
-            &ensp;Save the file as <b>&lt;project id&gt;dat</b>
+            In the <b>Input Files</b> <i>(middle)</i> section, click <b>[Browse]</b> and navigate to your dat file<br/>
+            On the right side click <b>[Report File]</b> and save it as <b>expand.txt</b> in the <b>[DATABASE]</b> folder. <br/>
+            In the <b>Output File</b> <i>(bottom left)</i> click <b>[Browse]</b> and navigate to the <b>[UNCLE]</b> folder. <br/>
+            &ensp;Save the file as <b>&lt;project number&gt;dat</b><br/>
+            Click <b>[Expand]</b> and wait for the program to finish creating the files.
           </p>
         </Accordion.Body>
       </Accordion.Item>
@@ -122,14 +124,14 @@ function Instructions() {
           <p><i>Note: There are 2 ways to do this. Copying the project from an older file and editing the data, or creating a new file from scratch.</i></p>
           <Accordion>
             <Accordion.Item eventKey="7-1">
-              <Accordion.Header>Step 1: Copy/Create</Accordion.Header>
+              <Accordion.Header>Step 1: Options</Accordion.Header>
               <Accordion.Body>
                 <Accordion>
                   <Accordion.Item  eventKey="7-1-1">
                     <Accordion.Header>Copy Uncle file:&nbsp;<i>Recommended for projects with standardized tables and processes (core/os, double weights)</i></Accordion.Header>
                     <Accordion.Body>
                       <p>
-                        Navigate to an earlier project and copy the <b>.j</b> and <b>.t</b> files.<br />
+                        Navigate to a previously completed project and copy the <b>.j</b> and <b>.t</b> files from the old <b>[UNCLE]</b> folder into the new <b>[UNCLE]</b> folder.<br />
                         <img src={jt} alt="jt" /><br/>
                       </p>
                     </Accordion.Body>
@@ -140,7 +142,8 @@ function Instructions() {
                       <p>
                         Press <b>Windows Key</b> and type Uncle<br/>
                         Press <b>Enter</b><br/>
-                        Browse to the project folder and save the uncle file as the project number.<br/>
+                        Click <b>[New Project]</b> and navigate to <b>i\PROJ\&lt;project number&gt;\DATAPROC\UNCLE</b><br/>
+                        Save the uncle file as the project number.
                       </p>
                     </Accordion.Body>
                   </Accordion.Item>
@@ -151,19 +154,20 @@ function Instructions() {
                 <Accordion.Header>Step 2: Adjust&nbsp;<b>Title</b>&nbsp;and&nbsp;<b>CaseID Position</b></Accordion.Header>
                 <Accordion.Body>
                   <p>
-                    Open the uncle file (<b>.j</b>)<br/>
+                    Open the uncle file (<b>.j</b>) if it is not already open<br/>
                     Click <b>[Project]</b> in the middle section of the ribbon.<br/>
-                    Edit the title text do the respective project name and date. <br/>
-                    <b>Title:</b> &CP <b>&lt;SURVEY NAME&gt;</b> // <b>&lt;MMM&gt;</b>. <b>&lt;DD&gt;</b> - <b>&lt;DD&gt;</b>, <b>&lt;YYYY&gt;</b> &RJ PAGE &PS<br/>
-                    <b>Case ID Position:</b><br/>
-                    &ensp;<b>First:</b> 1<br/>
-                    &ensp;<b>Last:</b> 10<br/>
+                    Edit the title text to the respective project name and date. <i>(Note: See screenshot below for example)</i> <br/>
+                    &emsp;<b>Title:</b> &CP <b>&lt;SURVEY NAME&gt;</b> // <b>&lt;MMM&gt;</b>. <b>&lt;DD&gt;</b> - <b>&lt;DD&gt;</b>, <b>&lt;YYYY&gt;</b> &RJ PAGE &PS<br/>
+                    &emsp;<b>Case ID Position:</b> Verify these are correct, even when copying.<br/>
+                    &emsp;&ensp;<b>First:</b> 1<br/>
+                    &emsp;&ensp;<b>Last:</b> 10<br/>
+                    Click <b>OK</b><br/>
                     <img src={projTitle} alt="Project/Title" /><br/>
                   </p>
                 </Accordion.Body>
               </Accordion.Item>
             <Accordion.Item  eventKey="7-3">
-              <Accordion.Header>Format of default tables</Accordion.Header>
+              <Accordion.Header>Default Tables Scripts: (Note: All carrots must be edited &lt;&gt;)</Accordion.Header>
               <Accordion.Body>
                <TableBody />
               </Accordion.Body>
@@ -172,9 +176,125 @@ function Instructions() {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="8">
-        <Accordion.Header>Step 9:</Accordion.Header>
+        <Accordion.Header>Step 9: <Tooltip title={cleanup_instructions}>&nbsp;Table Cleanup</Tooltip></Accordion.Header>
         <Accordion.Body>
-
+          <p>
+            Open the <b>tables.txt</b> file in the <b>[UNCLE]</b> folder.<br/>
+            Open the <b>Uncle</b> file (<b>.j</b>) if it is not already open<br/>
+            While inside of the <b>tables.txt</b> file, press <b>[ctrl+a]</b> then <b>[ctrl+c]</b><br/>
+            Navigate to the <b>Uncle</b> file and type <b>edit all</b>, then press <b>[ctrl+v]</b><br/>
+            <br/>
+            For simplicity sake, every step will begin with the same command <b>-> edit all</b>
+          </p>
+          <Accordion>
+            <Accordion.Item eventKey="8-1">
+              <Accordion.Header>
+                <Tooltip
+                  title="All D//S scores have an indentation with &AI2, this will indent 2 spaces. This indentation
+                    should only be included on lines that have a 'Total'.">
+                  Fixing Indentations and D//S scores
+                </Tooltip>
+              </Accordion.Header>
+            <Accordion.Body>
+              <p>
+                Useful Tips:<br/>
+                &emsp;Press <b>[ctrl+h]</b><br/>
+                <br/>
+                &emsp;&emsp;Find: <b>&AI2 UNS</b><br/>
+                &emsp;&emsp;Replace: <b>UNS</b><br/>
+                <br/>
+                &emsp;&emsp;Find: <b>&AI2 NO OPI</b><br/>
+                &emsp;&emsp;Replace: <b>NO OPI</b><br/>
+                <br/>
+                &emsp;&emsp;Find: <b>&AI2 OTH</b><br/>
+                &emsp;&emsp;Replace: <b>OTH</b><br/>
+                <br/>
+                &emsp;Press <b>[ctrl+f]</b> and search for <b>[</b> (bracket)<br/>
+                &emsp;&emsp;Replace the fill text with the appropriate
+                <span style={{"color": "#0000ff"}}>
+                  <Tooltip
+                    title="Common fills include
+                    more or less likely,
+                    support or oppose,
+                    agree or disagree,
+                    approve or disapprove,
+                    etc...">
+                    &nbsp;text
+                  </Tooltip>
+                </span>.<br/>
+                <br/>
+                <br/>
+                Some tables may require D//S scores but the output may not have added or indented them. The syntax is
+                as follows:<br/>
+                <pre>
+                  &emsp;R *D//S (AGREE - DISAGREE) ;NONE      ;EX (R3-R4) ;HP<br/>
+                  &emsp;R &UT- TOTAL AGREE         ;&lt;col&gt;-1:2 ;HP<br/>
+                  &emsp;R &UT- TOTAL DISAGREE      ;&lt;col&gt;-3:4 ;HP<br/>
+                  &emsp;R &AI2 STRONGLY AGREE      ;&lt;col&gt;-1   ;HP<br/>
+                  &emsp;R &AI2 SOMEWHAT AGREE      ;&lt;col&gt;-2   ;HP<br/>
+                  &emsp;R &AI2 SOMEWHAT DISAGREE   ;&lt;col&gt;-3   ;HP<br/>
+                  &emsp;R &AI2 STRONGLY DISAGREE   ;&lt;col&gt;-4   ;HP<br/>
+                  &emsp;R NO ANSWER                ;&lt;col&gt;N1:4 ;NOR SZR<br/>
+                </pre>
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+            <Accordion.Item eventKey="8-2">
+            <Accordion.Header>Fixing No Answer codes</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                In some instances the <b>No Answer</b> row will have choice codes that are not in proper order. These
+                issue may look like
+                <span style={{"color": "#0000ff"}}>
+                  <Tooltip
+                    title="R NO ANSWER ;NOTR(10:11:01,15:10,02,03,:04) ;NOR SZR">
+                    &nbsp;this&nbsp;
+                  </Tooltip>
+                </span>
+                when it should look
+                <span style={{"color": "#0000ff"}}>
+                  <Tooltip
+                    title="R NO ANSWER ;NOTR(10:11,1:15) ;NOR SZR">
+                    &nbsp;as follows&nbsp;
+                  </Tooltip>
+                </span>
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+            <Accordion.Item eventKey="8-3">
+                <Accordion.Header>
+                  <Tooltip
+                    title="Not all qualifiers will be properly added to each table, be sure to check the survey to verify
+                    what needs a qualifier. The qualifier label may also need to be changed.">
+                      Fixing Qualifiers
+                  </Tooltip>
+                </Accordion.Header>
+              <Accordion.Body>
+                <p>
+                  Qualifiers use the following syntax: <b>Q R(10:11,01)</b><br/>
+                  Default tables use the following syntax: <b>R BASE==TOTAL SAMPLE ;ALL ;HP NOVP</b><br/>
+                  Qualifier tables use the following syntax: <b>R BASE==TOTAL
+                  <span style={{"color": "#0000ff"}}>
+                    <Tooltip title="This may be changed to the qualifying criteria.">
+                      &nbsp;ASKED&nbsp;
+                    </Tooltip>
+                  </span>
+                  ;ALL ;HP NOVP</b>
+                </p>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="8-4">
+              <Accordion.Header>Identifying Ranks</Accordion.Header>
+              <Accordion.Body>
+                <p>
+                  Ranks use the following syntax: <b>O RANK</b><br/>
+                  <br/>
+                  We use ranks on tables where multiple choices are allowed. Unless specified otherwise, it is safe to
+                  assume all multi-choice tables will include ranking.
+                </p>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="9">
