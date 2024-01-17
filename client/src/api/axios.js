8761dc4feb5d7c28cc504be4e5ca031ca5f900da
MotoMatt5040 <motomatt5040@gmail.com>
2024-01-17 11:25:22 -0600

    Added .env info, added comment of actual working code for production. Do not use commented code for testing.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,3 +5,11 @@ export default axios.create({
   crossorigin: true,
   baseURL: process.env.REACT_APP_API_URL
 })
+
+// import axios from 'axios';
+//
+// export default axios.create({
+//   withCredentials: true,
+//   baseURL: "http://textingserver1.colo1.promarkresearch.com:5000"
+// })
+
