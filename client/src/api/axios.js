4d7c2aa6a41c3ac16d6d81cbd2384595a9b5adeb
MotoMatt5040 <motomatt5040@gmail.com>
2024-01-17 13:28:34 -0600

    reverted axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -6,10 +6,3 @@ export default axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
-// import axios from 'axios';
-//
-// export default axios.create({
-//   withCredentials: true,
-//   baseURL: "http://textingserver1.colo1.promarkresearch.com:5000"
-// })
-
