5c55989122d4f2dfc718df6dd8b23797aa4e0191
MotoMatt5040 <motomatt5040@gmail.com>
2024-02-02 11:09:17 -0600

    adjusted config to use environment variables

--- server/defaults/core/server_routes/config.py
+++ server/defaults/core/server_routes/config.py
@@ -3,22 +3,22 @@ import os
 import redis
 from dotenv import load_dotenv
 
-from ...utils.database.database import Database
+# from ...utils.database.database import Database
 
 load_dotenv()
 
 #  initialize database access info to connect to database
-dbai = Database()
+# dbai = Database()
 
 #  Build the connection string
 SQL_CONNECTION = 'DRIVER={};SERVER={};DATABASE={};UID={};PWD={}'.format(
-    dbai.driver,
-    dbai.server,
-    dbai.database,
-    dbai.user_id,
-    dbai.password
+    '{ODBC Driver 17 for SQL Server}',
+    os.environ.get('coreserver'),
+    os.environ.get('portal_database'),
+    os.environ.get("coreuser"),
+    os.environ.get("corepassword")
 )
-del dbai
+# del dbai
 uri = f'mssql+pyodbc:///?odbc_connect={SQL_CONNECTION}'
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
@@ -30,6 +30,7 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://localhost:6379")  # DO NOT COMMIT/PUSH THIS LINE
+    # SESSION_REDIS = redis.from_url("redis://redis:6379")  # FOR PRODUCTION/DEV
+    SESSION_REDIS = redis.from_url("redis://localhost:6379")  # FOR TESTING
     SESSION_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SECURE = True
