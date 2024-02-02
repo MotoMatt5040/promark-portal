05b20f764f57804200d9afbb6dc1ac218bb36686
MotoMatt5040 <motomatt5040@gmail.com>
2024-02-02 11:09:00 -0600

    default queries added

---
+++ server/defaults/core/dashboard/defaultQueries.py
@@ -0,0 +1,20 @@
+from datetime import date, timedelta
+
+from server.defaults.core.auth.permissions import Permissions
+
+
+def active_project_query():
+    return \
+        f'SELECT DISTINCT projectid FROM tblProduction ' \
+        f"WHERE recdate >= '{date.today() - timedelta(hours=8)}'"
+
+
+def active_location_query():
+    return "SELECT LongName, LocationID FROM tblLocation WHERE TYPE = 'P' AND ACTIVE = 1 AND CATIModule# is not null"
+    match Permissions.authorization_location:
+        case '101':
+            return "SELECT LongName FROM tblLocation WHERE TYPE = 'P' AND ACTIVE = 1  AND CATIModule# is not null"
+
+
+def check_permissions():
+    return "SELECT [FAJITA].[dbo].[users] location, authorization_level FROM tblUsers"
