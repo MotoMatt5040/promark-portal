712e87213f64059c0aff6081bd1f60abec8413f0
MotoMatt5040 <motomatt5040@gmail.com>
2024-02-06 13:58:24 -0600

    added request and response for updating periodic update table

--- server/defaults/core/server_routes/periodicUpdateRoutes.py
+++ server/defaults/core/server_routes/periodicUpdateRoutes.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, request, make_response
 from server.defaults.core.dashboard.periodic_update.periodicUpdate import PeriodicUpdate
 from server.defaults.utils.database.datapuller import DataPuller
 from server.defaults.core.dashboard.defaultQueries import active_location_query
 import json
+import os
+
+allowed_domain = os.environ["testing"]
 
 
 pu = PeriodicUpdate()
@@ -10,17 +13,43 @@ periodic_update = Blueprint('periodic', __name__)
 dp = DataPuller()
 
 
-@periodic_update.route('/periodic_update')
+@periodic_update.route('/periodic_update', methods=['POST', 'OPTIONS'])
 def updater():
 
-    pu.projectid = request.json['projectid']
-    pu.location = request.json['location']
+    if request.method == "OPTIONS":  # CORS preflight
+        return _build_cors_preflight_response()
 
-    return pu.update()
+    pu.projectid = request.json['projectID']
+    pu.location = request.json['locationID']
+
+    response = pu.update().to_json(orient='records')
+
+    return response
+
 
 @periodic_update.route('/active/locations', methods=['GET'])
 def active_locations():
     response = pu.active_locations()
-    print(response.to_string())
     # print(response)
-    return response.to_json()#.to_dict('records')
+    # print(response.to_string())
+    response.set_index('LongName', inplace=True)
+    response = response.to_dict()['LocationID']
+    # print(json.dumps(response.to_dict()['LocationID'],indent=4))
+    # print(json.dumps(json.loads(response.to_json()), indent=4))
+    print(response)
+    return response#.to_dict('records')
+
+
+@periodic_update.route('/active/projects', methods=['GET'])
+def active_projects():
+    response = pu.active_projectIDs()
+    return response.to_json()
+
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add('Access-Control-Allow-Origin', allowed_domain)
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    response.headers.add('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
