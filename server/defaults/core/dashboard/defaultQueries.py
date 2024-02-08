5bd24168ccd0ffd31a8a08e703514ec070985d27
MotoMatt5040 <motomatt5040@gmail.com>
2024-02-08 14:29:08 -0600

    updated default queries

--- server/defaults/core/dashboard/defaultQueries.py
+++ server/defaults/core/dashboard/defaultQueries.py
@@ -1,19 +1,14 @@
 from datetime import date, timedelta
 
-from server.defaults.core.auth.permissions import Permissions
-
-
 def active_projectIDs_query():
     return \
         f'SELECT DISTINCT projectid FROM tblHourlyProduction ' \
-        f"WHERE lastupdate >= '{date.today() - timedelta(hours=8)}'"
+        f"WHERE lastupdate >= '2023-03-15'"
+        # f"WHERE lastupdate >= '{date.today() - timedelta(hours=8)}'"
 
 
 def active_location_query():
     return "SELECT LongName, LocationID FROM tblLocation WHERE TYPE = 'P' AND ACTIVE = 1 AND CATIModule# is not null"
-    match Permissions.authorization_location:
-        case '101':
-            return "SELECT LongName FROM tblLocation WHERE TYPE = 'P' AND ACTIVE = 1  AND CATIModule# is not null"
 
 
 def check_permissions():
