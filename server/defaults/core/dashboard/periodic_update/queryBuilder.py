576c037222ab4dbb64ffb86dec4c974f4fd56e5f
MotoMatt5040 <motomatt5040@gmail.com>
2024-02-13 11:10:11 -0600

    adjuusted query to accept "All" options for recloc and project id
    fixed resets to not override eachother when selection locations and projects

--- server/defaults/core/dashboard/periodic_update/queryBuilder.py
+++ server/defaults/core/dashboard/periodic_update/queryBuilder.py
@@ -8,11 +8,12 @@ class QueryBuilder:
         self._projectidclause = ''
         self._projectidclause2 = ''
         self._projectidclause3 = ''
-        self._allclause = 'tblHourlyProductionDetail.projectid, '
+        self._projectIDAllClause = ''
 
     def periodic_update_query(self) -> str:
+        print(self._projectIDAllClause, ": ALL CLAUSE")
         return \
-            f"SELECT {self._allclause}RecLoc, tblCC3EMployeeList.EID, LastName + ', ' + FirstName AS MyName, " \
+            f"SELECT {self._projectIDAllClause}{self._locationAllClause}tblCC3EMployeeList.EID, LastName + ', ' + FirstName AS MyName, " \
             f"Tenure, HRS, CMS, IntAVG AS IntAL, CPH, " \
             "MPH, PauseTime, ConnectTime, CodeQty AS TotalDials, NAAM " \
             "FROM tblHourlyProductionDetail INNER JOIN tblCC3EmployeeList " \
@@ -38,6 +39,10 @@ class QueryBuilder:
 
     @location.setter
     def location(self, location):
+        self.resetLocation()
+        if location == 'All':
+            self._locationAllClause = f"RecLoc, "
+            return
         self._location = location
         self._locationClause = f" AND tblHourlyProductionDetail.recLoc = '{location}'"
 
@@ -47,20 +52,27 @@ class QueryBuilder:
 
     @projectid.setter
     def projectid(self, projectid):
+        self.resetProjectID()
+        if projectid == "All":
+            self._projectIDAllClause = 'tblHourlyProductionDetail.projectid, '
+            return
         self._projectid = projectid
         self._projectidclause = f" AND projectID = '{projectid}' "
         self._projectidclause2 = f"WHERE projectID = '{projectid}' "
         self._projectidclause3 = f" tblHourlyProductionDetail.projectID = '{projectid}' AND "
-        self._allclause = ''
+
 
     '-----reset variables-----(may not be needed)'
 
-    def reset(self):
-        self._locationClause = None
+    def resetProjectID(self):
         self._projectid = None
-        self._location = None
         self._projectidclause = ''
         self._projectidclause2 = ''
         self._projectidclause3 = ''
-        self._allclause = 'tblHourlyProductionDetail.projectid, '
-        self._qry = None
+        self._projectIDAllClause = ''
+
+    def resetLocation(self):
+        self._locationClause = None
+        self._location = None
+        self._locationClause = ''
+        self._locationAllClause = ''
