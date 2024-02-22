72d72ea55f4b9f1c0c39cf7ef468e7f3055c95dc
MotoMatt5040 <motomatt5040@gmail.com>
2024-02-22 14:28:45 -0600

    initial commit for quota api

---
+++ server/defaults/core/quota_management/api.py
@@ -0,0 +1,57 @@
+import os
+import requests
+
+class API:
+
+    def __init__(self):
+        self._acuity_access_token = os.environ['access_token']
+        self._voxco_access_token = None  # this token must be refreshed every hour, easier to do upon update
+        self._web_sid = None
+        self._landline_sid = None
+        self._cell_sid = None
+        self._web_url = None
+        self._landline_url = None
+        self._cell_url = None
+
+    @property
+    def web_sid(self):
+        return self._web_sid
+
+    @property
+    def web_url(self):
+        return self._web_url
+
+    @web_sid.setter
+    def web_sid(self, sid):
+        self._web_sid = sid
+        self._web_url = f"{os.environ['web_quotas_url']}{sid}"
+
+    @property
+    def landline_sid(self):
+        return self._landline_sid
+
+    @property
+    def landline_url(self):
+        return self._landline_url
+
+    @landline_sid.setter
+    def landline_sid(self, sid):
+        self._landline_sid = sid
+        self._landline_url = f"{os.environ['voxco_quotas_url']}{sid}/stratas?status=All"
+
+    @property
+    def cell_sid(self):
+        return self._cell_sid
+
+    @property
+    def cell_url(self):
+        return self._cell_url
+
+    @cell_sid.setter
+    def cell_sid(self, sid):
+        self._cell_sid = sid
+        self._cell_url = f"{os.environ['voxco_quotas_url']}{sid}/stratas?status=All"
+
+    @property
+    def voxco_access_token(self):
+        return requests.get(os.environ['voxco_access_token_url']).json()['Token']
