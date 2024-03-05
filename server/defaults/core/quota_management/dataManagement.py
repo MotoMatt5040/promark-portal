d36b45f577fca6a867ed1ef523ccdb1304bf36a8
MotoMatt5040 <motomatt5040@gmail.com>
2024-03-05 11:32:09 -0600

    added data merger

--- server/defaults/core/quota_management/dataManagement.py
+++ server/defaults/core/quota_management/dataManagement.py
@@ -15,7 +15,8 @@ class DataManagement(API):
         self._source = None
 
     def set_data(self):
-        df = pd.DataFrame(self.create_layout())
+        data = self.create_layout()
+        df = pd.DataFrame(data)
         match self.source:
             case "web":
                 self._web_data = df
@@ -67,7 +68,7 @@ class DataManagement(API):
                 for item in data:
                     output['StratumId'].append(item['StratumId'])
                     output['Status'].append(item['Status'])
-                    output['Criterion'].append(item['Criterion'])
+                    output['Criterion'].append(item['Criterion'].replace(" AND STYPE>2", ''))
                     output['Objective'].append(item['Objective'])
                     output['Frequency'].append(item['Frequency'])
                     output['To Do'].append((item['Objective'] - item['Frequency']) if item['Objective'] > 0 else 0)
@@ -75,7 +76,7 @@ class DataManagement(API):
                 for item in data:
                     output['StratumId'].append(item['Position'])
                     output['Status'].append('Open' if item['Status'] == 0 else 'Closed')
-                    output['Criterion'].append(item['Criterion'])
+                    output['Criterion'].append(item['Criterion'].replace(" AND STYPE=2", ''))
                     output['Objective'].append(item['Quota'])
                     output['Frequency'].append(item['Frequence'])
                     output['To Do'].append((item['Quota'] - item['Frequence']) if item['Quota'] > 0 else 0)
@@ -98,16 +99,49 @@ class DataManagement(API):
 
     def clean_names(self):
         df = self.set_data()
-        # df['Criterion'] = df['Criterion'].str.split(' ').str[0].tolist()
         return df
 
     def merge_data(self):
         df1 = self._web_data
         df2 = self._landline_data
         df3 = self._cell_data
-        d = df2.merge(df1[['Criterion']])
+
+        d = pd.merge(df1, df2, on=["Criterion"])
+        d['Objective'] = d['Objective_x'] + d['Objective_y']
+        d['Frequency'] = d['Frequency_x'] + d['Frequency_y']
+        d['To Do'] = d["To Do_x"] + d['To Do_y']
+        d.drop(['Objective_x', 'Objective_y', 'Frequency_x', 'Frequency_y', 'To Do_x', 'To Do_y'], axis=1, inplace=True)
+
+        d2 = pd.merge(d, df3, on="Criterion")
+        d2['Objective'] = d2['Objective_x'] + d2['Objective_y']
+        d2['Frequency'] = d2['Frequency_x'] + d2['Frequency_y']
+        d2['To Do'] = d2["To Do_x"] + d2['To Do_y']
+        d2.drop(['Objective_x', 'Objective_y', 'Frequency_x', 'Frequency_y', 'To Do_x', 'To Do_y'], axis=1, inplace=True)
+
+        d2.rename(columns={
+            "StratumId_x": "Web StratumId",
+            "Status_x": "Web Status",
+            "StratumId_y": "LL StratumId",
+            "Status_y": "LL Status",
+            "StratumId": "Cell StratumId",
+            "Status": "Cell Status"
+        }, inplace=True)
 
 
+        df = d2[[
+            'Web StratumId',
+            'Web Status',
+            'LL StratumId',
+            'LL Status',
+            'Cell StratumId',
+            'Cell Status',
+            'Criterion',
+            'Objective',
+            'Frequency',
+            'To Do'
+        ]]
+
+        return df
 
     @property
     def source(self):
