87ac05e73f1554bad1accb931473784a40365aef
MotoMatt5040 <motomatt5040@gmail.com>
2024-03-08 10:41:33 -0600

    changed environment variable for axios url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   withCredentials: true,
   crossorigin: true,
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_TEST_API_URL
 })
 
