1bbd00f8f60a24b855d7ebadb0f41b403b3e4b2c
MotoMatt5040 <motomatt5040@gmail.com>
2024-03-14 13:16:12 -0500

    new login page styling

--- server/defaults/core/auth/models.py
+++ server/defaults/core/auth/models.py
@@ -3,11 +3,24 @@ from uuid import uuid4
 
 db = SQLAlchemy()
 
-def get_uuid():
-    return uuid4().hex
-
 class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid())
-    email = db.Column(db.String(345), unique=True)
+    __tablename__ = "tblPortalUsers"
+    empid = db.Column(db.Text, unique=True, nullable=True)
+
+    email = db.Column(db.String(345), primary_key=True, unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+    salt = db.Column(db.Text, nullable=False)
+    temp_password = db.Column(db.Text, nullable=True)
+
+    twofactor_enabled = db.Column(db.Text, nullable=True)  # 0 disabled, 1 phone, 2 email, 3 both
+    phone = db.Column(db.Text, nullable=True)
+
+    secret_question_one = db.Column(db.Text, nullable=True)
+    secret_question_one_salt = db.Column(db.Text, nullable=True)
+    secret_answer_one = db.Column(db.Text, nullable=True)
+    secret_answer_one_salt = db.Column(db.Text, nullable=True)
+
+    secret_question_two = db.Column(db.Text, nullable=True)
+    secret_question_two_salt = db.Column(db.Text, nullable=True)
+    secret_answer_two = db.Column(db.Text, nullable=True)
+    secret_answer_two_salt = db.Column(db.Text, nullable=True)
