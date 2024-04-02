971193e90febf389b444347c68b45c2d69e36410
MotoMatt5040 <motomatt5040@gmail.com>
2024-04-02 09:51:38 -0500

    updated header for login status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
+// TODO Filter navbar elementes based on appropriate user roles
+// TODO adjust how the login/profile bar works
+
+
 const Header = (props) => {
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -20,12 +26,16 @@ const Header = (props) => {
             </NavDropdown>
             <Nav.Link href="data_processing">Data Processing</Nav.Link>
             <Nav.Link href="global_quotas">Global Quota Module</Nav.Link>
+            {localStorage.getItem("logged_in") === 'true'
+            ?
             <NavDropdown title="Profile" id="profile">
               <NavDropdown.Item href="settings">Settings</NavDropdown.Item>
-              <NavDropdown.Item href="https://www.promarkresearch.com/privacy-policy/">Privacy Policy</NavDropdown.Item>
-              <div style={{borderBottom: "1px solid lightgrey", margin: "5px"}}></div>
+              <NavDropdown.Divider/>
               <NavDropdown.Item href="logout">Logout</NavDropdown.Item>
             </NavDropdown>
+            :
+            <Nav.Link href="login">Sign In</Nav.Link>
+            }
           </Nav>
         </Navbar.Collapse>
       </Container>
