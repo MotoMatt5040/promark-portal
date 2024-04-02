57588f638e221e1c9b2f786000f0badbe9ff9349
MotoMatt5040 <motomatt5040@gmail.com>
2024-04-02 17:22:31 -0500

    adjust header to replace url instead of append

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,32 +17,36 @@ const Header = (props) => {
     return () => clearTimeout(delay);
   }, []); // Run only once after initial render
 
+  const replaceCurrentUrl = (url) => {
+    window.location.replace(url);
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/home">Promark Research</Navbar.Brand>
+        <Navbar.Brand onClick={() => replaceCurrentUrl("/home")}>Promark Research</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="https://dashapp.promarkresearch.com/">Dashboard</Nav.Link>
             <NavDropdown title="Production Dashboard" id="dash">
-              <NavDropdown.Item href="/production_report">Production Report</NavDropdown.Item>
-              <NavDropdown.Item href="/periodic_update">Periodic Update</NavDropdown.Item>
-              <NavDropdown.Item href="/interviewer_breakdown">Interviewer Breakdown</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => replaceCurrentUrl("/production_report")}>Production Report</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => replaceCurrentUrl("/periodic_update")}>Periodic Update</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => replaceCurrentUrl("/interviewer_breakdown")}>Interviewer Breakdown</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="data_processing">Data Processing</Nav.Link>
-            <Nav.Link href="global_quotas">Global Quota Module</Nav.Link>
+            <Nav.Link onClick={() => replaceCurrentUrl("/data_processing")}>Data Processing</Nav.Link>
+            <Nav.Link onClick={() => replaceCurrentUrl("/global_quotas")}>Global Quota Module</Nav.Link>
             {isLoading ? (
               <Nav.Link disabled>Loading...</Nav.Link>
             ) : (
               localStorage.getItem("logged_in") === 'true' ? (
                 <NavDropdown title="Profile" id="profile">
-                  <NavDropdown.Item href="settings">Settings</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() => replaceCurrentUrl("/settings")}>Settings</NavDropdown.Item>
                   <NavDropdown.Divider/>
-                  <NavDropdown.Item href="logout">Logout</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() => replaceCurrentUrl("/logout")}>Logout</NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <Nav.Link href="login" style={{color: 'blue'}}>Sign In</Nav.Link>
+                <Nav.Link onClick={() => replaceCurrentUrl("/login")} style={{color: 'blue'}}>Sign In</Nav.Link>
               )
             )}
           </Nav>
