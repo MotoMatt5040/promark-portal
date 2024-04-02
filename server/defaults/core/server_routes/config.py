1a7aee2c9b79e10b221a622e9b22fce2042b422f
MotoMatt5040 <motomatt5040@gmail.com>
2024-04-02 09:43:02 -0500

    adjust config for server

--- server/defaults/core/server_routes/config.py
+++ server/defaults/core/server_routes/config.py
@@ -7,13 +7,15 @@ allowed_domain = os.environ['testing']
 
 #  initialize database access info to connect to database
 
+#  TODO update to Driver 18
+
 #  Build the connection string
 SQL_CONNECTION = 'DRIVER={};SERVER={};DATABASE={};UID={};PWD={}'.format(
     '{ODBC Driver 17 for SQL Server}',
-    os.environ.get('coreserver'),
-    os.environ.get('portal_database'),
-    os.environ.get("coreuser"),
-    os.environ.get("corepassword")
+    os.environ['coreserver'],
+    os.environ['caligula'],
+    os.environ["coreuser"],
+    os.environ["corepassword"]
 )
 # del dbai
 uri = f'mssql+pyodbc:///?odbc_connect={SQL_CONNECTION}'
@@ -31,3 +33,6 @@ class ApplicationConfig:
     SESSION_REDIS = redis.from_url("redis://localhost:6379")  # FOR TESTING
     SESSION_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_PATH = '/'
+    SESSION_FILE_DIR = '/'
