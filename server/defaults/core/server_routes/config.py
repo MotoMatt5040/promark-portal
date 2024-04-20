4fe5cedeaafff8382446f832933cdace4c513954
MotoMatt5040 <motomatt5040@gmail.com>
2024-04-20 12:01:55 -0500

    turn echo off for sql

--- server/defaults/core/server_routes/config.py
+++ server/defaults/core/server_routes/config.py
@@ -23,7 +23,7 @@ class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_DATABASE_URI = uri
 
     SESSION_TYPE = "redis"
