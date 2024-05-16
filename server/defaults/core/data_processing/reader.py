355ab09738df1ebdfd9ce0ba02065112e47dc55d
MotoMatt5040 <motomatt5040@gmail.com>
2024-05-16 12:14:15 -0500

    formatting

--- server/defaults/core/data_processing/reader.py
+++ server/defaults/core/data_processing/reader.py
@@ -19,18 +19,15 @@ class Reader:
     def setUrl(self, survey_id: str):
         self.api.set_sid(survey_id)
 
+    def get_survey_name(self):
+        return self.api.get_survey_name()
+
     def request_data(self):
         match self.api.request_data():
             case 'order dne':
                 return -1
             case _:
                 return 1
-
-    def get_survey_name(self):
-        return self.api.get_survey_name()
-
-    def get_questions(self):
-        return self.api.question_names()
 
     def get_order(self):
         res = self.api.order()
@@ -95,26 +92,26 @@ class Reader:
         |_TOTAL_DURATION_(SEC)______|___5___|
         |_VEND______________________|___1___|
 
-R *D//S (REPUBLICAN - DEMOCRAT)       ;NONE   ;EX (R3-R5)
-R &UT- TOTAL REPUBLICAN               ;61-1:2
-R &UT- TOTAL INDEPENDENT              ;61-3:5
-R &UT- TOTAL DEMOCRAT                 ;61-6:7
-
-T QIDEOLOGY:
-T Would you consider your political views to be conservative, liberal, or moderate?
-T
-T /
-R BASE==TOTAL SAMPLE             ;ALL     ;HP NOVP
-R *D//S (CONSERVATIVE - LIBERAL) ;NONE    ;EX (R3-R4)
-R &UT- TOTAL CONSERVATIVE        ;112-1:2
-R &UT- TOTAL LIBERAL             ;112-4:5
-R &AI2 STRONGLY CONSERVATIVE     ;112-1
-R &AI2 SOMEWHAT CONSERVATIVE     ;112-2
-R MODERATE                       ;112-3
-R &AI2 SOMEWHAT LIBERAL          ;112-4
-R &AI2 STRONGLY LIBERAL          ;112-5
-R UNSURE // REFUSED              ;112-6
-R NO ANSWER                      ;112N1:6 ;NOR SZR
+        R *D//S (REPUBLICAN - DEMOCRAT)       ;NONE   ;EX (R3-R5)
+        R &UT- TOTAL REPUBLICAN               ;61-1:2
+        R &UT- TOTAL INDEPENDENT              ;61-3:5
+        R &UT- TOTAL DEMOCRAT                 ;61-6:7
+        
+        T QIDEOLOGY:
+        T Would you consider your political views to be conservative, liberal, or moderate?
+        T
+        T /
+        R BASE==TOTAL SAMPLE             ;ALL     ;HP NOVP
+        R *D//S (CONSERVATIVE - LIBERAL) ;NONE    ;EX (R3-R4)
+        R &UT- TOTAL CONSERVATIVE        ;112-1:2
+        R &UT- TOTAL LIBERAL             ;112-4:5
+        R &AI2 STRONGLY CONSERVATIVE     ;112-1
+        R &AI2 SOMEWHAT CONSERVATIVE     ;112-2
+        R MODERATE                       ;112-3
+        R &AI2 SOMEWHAT LIBERAL          ;112-4
+        R &AI2 STRONGLY LIBERAL          ;112-5
+        R UNSURE // REFUSED              ;112-6
+        R NO ANSWER                      ;112N1:6 ;NOR SZR
 
         """
 
@@ -181,3 +178,7 @@ R NO ANSWER                      ;112N1:6 ;NOR SZR
                                 "R BASE==TOTAL SAMPLE             ;ALL     ;HP NOVP\n"
                             )
                     print("Main loop:", writer.qname, err)
+
+
+    def get_questions(self):
+        return self.api.question_names()
