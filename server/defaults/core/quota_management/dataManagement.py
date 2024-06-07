62c288cab59debad54f8ab33455db100476ef15c
MotoMatt5040 <motomatt5040@gmail.com>
2024-06-06 23:00:58 -0500

    adjusted match case statement and changed to use for loop and if statements for layout

--- server/defaults/core/quota_management/dataManagement.py
+++ server/defaults/core/quota_management/dataManagement.py
@@ -18,17 +18,16 @@ class DataManagement(API):
         self._cell_data = pd.DataFrame()
         self._source = None
 
-
     def set_data(self):
         df = pd.DataFrame(self.create_layout())
         match self.source:
-            case "com":
+            case "COM":
                 self._com_data = df
-            case "web":
+            case "Web":
                 self._web_data = df
-            case "landline":
+            case "LL":
                 self._landline_data = df
-            case "cell":
+            case "Cell":
                 self._cell_data = df
             case _:
                 self._com_data = pd.DataFrame()
@@ -39,99 +38,68 @@ class DataManagement(API):
 
     def create_layout(self):
         data = self.get_data()
+        output = {
+            f'{self.source} StratumId': [],
+            f'{self.source} Status': [],
+            f'Criterion': [],
+            f'{self.source} Objective': [],
+            f'{self.source} Frequency': [],
+            f'{self.source} To Do': []
+        }
+        if self.source == 'COM':
+            output['COM Label'] = []
+        elif self.source == 'Web':
+            output['Web Label'] = []
 
-        match self.source:  # sift through the data to pull the data we want into a dict
-            case "com":
-                output = {
-                    'COM StratumId': [],
-                    'COM Status': [],
-                    'Criterion': [],
-                    'COM Objective': [],
-                    'COM Frequency': [],
-                    'COM To Do': []
-                }
-                for item in data:
-                    output['COM StratumId'].append(item['Position'])
-                    output['COM Status'].append('Open' if item['Status'] == 0 else 'Closed')
-                    output['Criterion'].append(item['Criterion'].strip())  # STYPE is explicit, not needed in our text
-                    # print(item['Criterion'], item['Criterion'].length())
-                    output['COM Objective'].append(item['Quota'])
-                    output['COM Frequency'].append(item['Frequence'])
-                    output['COM To Do'].append((item['Quota'] - item['Frequence']) if item['Quota'] > 0 else 0)  # i dont know why i did this in this way
-            case 'web':
-                output = {
-                    'Web StratumId': [],
-                    'Web Status': [],
-                    'Criterion': [],
-                    'Web Objective': [],
-                    'Web Frequency': [],
-                    'Web To Do': []
-                }
-                for item in data:
-                    output['Web StratumId'].append(item['StratumId'])
-                    output['Web Status'].append(item['Status'])
-                    output['Criterion'].append(item['Criterion']
-                                               .replace(" AND STYPE>2", '')
-                                               .replace(" AND STYPE=3", '')
-                                               .replace(" AND STYPE=4", '')
-                                               )  # STYPE is explicit, not needed in our text
-                    output['Web Objective'].append(item['Objective'])
-                    output['Web Frequency'].append(item['Frequency'])
-                    output['Web To Do'].append((item['Objective'] - item['Frequency']) if item['Objective'] > 0 else 0)  # i dont know why i did this in this way
-            case 'landline':
-                output = {
-                    'LL StratumId': [],
-                    'LL Status': [],
-                    'Criterion': [],
-                    'LL Objective': [],
-                    'LL Frequency': [],
-                    'LL To Do': []
-                }
-                for item in data:
-                    output['LL StratumId'].append(item['Position'])
-                    output['LL Status'].append('Open' if item['Status'] == 0 else 'Closed')
-                    output['Criterion'].append(item['Criterion'])
-                    output['LL Objective'].append(item['Quota'])
-                    output['LL Frequency'].append(item['Frequence'])
-                    output['LL To Do'].append((item['Quota'] - item['Frequence']) if item['Quota'] > 0 else 0)  # i dont know why i did this in this way
-            case 'cell':
-                output = {
-                    'Cell StratumId': [],
-                    'Cell Status': [],
-                    'Criterion': [],
-                    'Cell Objective': [],
-                    'Cell Frequency': [],
-                    'Cell To Do': []
-                }
-                for item in data:
-                    output['Cell StratumId'].append(item['Position'])
-                    output['Cell Status'].append('Open' if item['Status'] == 0 else 'Closed')
-                    output['Criterion'].append(item['Criterion'].replace(" AND STYPE=2", ''))  # STYPE is explicit, not needed in our text
-                    output['Cell Objective'].append(item['Quota'])
-                    output['Cell Frequency'].append(item['Frequence'])
-                    output['Cell To Do'].append((item['Quota'] - item['Frequence']) if item['Quota'] > 0 else 0)  # i dont know why i did this in this way
-            case _:
+        print(json.dumps(output, indent=4))
+
+        for item in data:
+            if self.source != 'Web':
+                output[f'{self.source} StratumId'].append(item['Position'])
+                output[f'{self.source} Status'].append('Open' if item['Status'] == 0 else 'Closed')
+                if self.source == 'COM':
+                    output['COM Label'].append(item['Label'])
+                if self.source == "COM" or self.source == "LL":
+                    output['Criterion'].append(item['Criterion'].strip())
+                if self.source == 'Cell':
+                    output['Criterion'].append(item['Criterion'].replace(" AND STYPE=2", '').strip())
+                output[f'{self.source} Objective'].append(item['Quota'])
+                output[f'{self.source} Frequency'].append(item['Frequence'])
+                output[f'{self.source} To Do'].append((item['Quota'] - item['Frequence']) if item['Quota'] > 0 else 0)
+            elif self.source == 'Web':
+                output['Web StratumId'].append(item['StratumId'])
+                output['Web Status'].append(item['Status'])
+                output['Web Label'].append(item['Label'])
+                output['Criterion'].append(item['Criterion']
+                                           .replace(" AND STYPE>2", '')
+                                           .replace(" AND STYPE=3", '')
+                                           .replace(" AND STYPE=4", ''))
+                output['Web Objective'].append(item['Objective'])
+                output['Web Frequency'].append(item['Frequency'])
+                output['Web To Do'].append((item['Objective'] - item['Frequency']) if item['Objective'] > 0 else 0)
+            else:
                 return pd.DataFrame()
+
         return output
 
     def get_data(self):
         match self.source:
-            case "com":
+            case "COM":
                 return requests.get(
                     self.com_quotas_url,
                     headers={"Authorization": f"Client {self.voxco_access_token}"}
                 ).json()
-            case "web":
+            case "Web":
                 return requests.get(
                     self.web_quotas_url,
                     headers={"Authorization": f"Client {self._acuity_access_token}"}
                 ).json()
-            case "landline":
+            case "LL":
                 return requests.get(
                     self.landline_quotas_url,
                     headers={"Authorization": f"Client {self.voxco_access_token}"}
                 ).json()
-            case "cell":
+            case "Cell":
                 return requests.get(
                     self.cell_quotas_url,
                     headers={"Authorization": f"Client {self.voxco_access_token}"}
@@ -141,29 +109,34 @@ class DataManagement(API):
 
     def survey_name(self):
         match self.source:
-            case 'com':
+            case "COM":
                 r = requests.get(self.com_survey_url, headers={"Authorization": f"Client {self.voxco_access_token}"}).json()
                 return f"{r['Name']} {r['Description']}"
-            case 'web':
+            case "Web":
                 return requests.get(self.web_survey_url, headers={"Authorization": f"Client {self._acuity_access_token}"}).json()['Name']
-            case 'landline':
+            case "LL":
                 r = requests.get(self.landline_survey_url, headers={"Authorization": f"Client {self.voxco_access_token}"}).json()
                 return f"{r['Name']} {r['Description']}"
-            case 'cell':
+            case "Cell":
                 r = requests.get(self.cell_survey_url, headers={"Authorization": f"Client {self.voxco_access_token}"}).json()
                 return f"{r['Name']} {r['Description']}"
             case _:
                 return
-
-    def clean_names(self):
-        df = self.set_data()
-        return df
 
     def merge_data(self):
         df_com = self._com_data
         df_web = self._web_data
         df_ll = self._landline_data
         df_cell = self._cell_data
+
+        print("\033[93m\ncom\033[0m")
+        print(df_com.to_string())
+        print("\033[93m\nweb\033[0m")
+        print(df_web.to_string())
+        print("\033[93m\nlandline\033[0m")
+        print(df_ll.to_string())
+        print("\033[93m\ncell\033[0m")
+        print(df_cell.to_string())
 
         # print(df_com.to_string())
         # print(df_web.to_string())
@@ -176,9 +149,13 @@ class DataManagement(API):
             print(traceback.format_exc())
             print(err)
 
+        df_cwlc.fillna(0, inplace=True)
+
         df_cwlc['W%'] = np.round(df_cwlc['Web Frequency'] * 100 / df_cwlc['COM Frequency'], 2)
         df_cwlc['L%'] = np.round(df_cwlc['LL Frequency'] * 100 / df_cwlc['COM Frequency'], 2)
         df_cwlc['C%'] = np.round(df_cwlc['Cell Frequency'] * 100 / df_cwlc['COM Frequency'], 2)
+        df_cwlc['COM Frequency'] = df_cwlc['Web Frequency'] + df_cwlc['LL Frequency'] + df_cwlc['Cell Frequency']
+        df_cwlc['COM To Do'] = df_cwlc['COM Objective'] - df_cwlc['COM Frequency']
 
         self.reset()
 
