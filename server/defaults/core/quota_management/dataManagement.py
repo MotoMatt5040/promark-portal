71629d84cf4ae83be5245dd8a43e93a711eb8e99
MotoMatt5040 <motomatt5040@gmail.com>
2024-06-10 13:18:50 -0500

    Added t2w and panel data with calculations

--- server/defaults/core/quota_management/dataManagement.py
+++ server/defaults/core/quota_management/dataManagement.py
@@ -8,23 +8,31 @@ import numpy as np
 import requests
 import json
 
+
 class DataManagement(API):
 
     def __init__(self):
         super().__init__()
         self._com_data = pd.DataFrame()
         self._web_data = pd.DataFrame()
+        self._panel_data = pd.DataFrame()
+        self._t2w_data = pd.DataFrame()
         self._landline_data = pd.DataFrame()
         self._cell_data = pd.DataFrame()
         self._source = None
 
-    def set_data(self):
-        df = pd.DataFrame(self.create_layout())
+    def set_data(self):  # TODO panel and t2w
+        if self.source != 'Web':
+            df = pd.DataFrame(self.create_layout())
+
         match self.source:
             case "COM":
                 self._com_data = df
             case "Web":
-                self._web_data = df
+                self._panel_data, self._t2w_data = pd.DataFrame(self.create_layout()[0]), pd.DataFrame(self.create_layout()[1])
+                # print(self._panel_data.to_string())
+                # print(self._t2w_data.to_string())
+                return self._panel_data, self._t2w_data
             case "LL":
                 self._landline_data = df
             case "Cell":
@@ -32,12 +40,21 @@ class DataManagement(API):
             case _:
                 self._com_data = pd.DataFrame()
                 self._web_data = pd.DataFrame()
+                self._panel_data = pd.DataFrame()
+                self._t2w_data = pd.DataFrame()
                 self._landline_data = pd.DataFrame()
                 self._cell_data = pd.DataFrame()
+
+        # print(df.to_string())
         return df
 
     def create_layout(self):
         data = self.get_data()
+        # if self.source == "COM":
+            # print(self.source)
+            # print(self.com_sid)
+            # # print(self.sid)
+            # print(json.dumps(data, indent=4))
         output = {
             f'{self.source} StratumId': [],
             f'{self.source} Status': [],
@@ -49,9 +66,27 @@ class DataManagement(API):
         if self.source == 'COM':
             output['COM Label'] = []
         elif self.source == 'Web':
-            output['Web Label'] = []
-
-        print(json.dumps(output, indent=4))
+            # output['Web Label'] = []
+            output = {
+                'panel': {
+                    f'Panel StratumId': [],
+                    f'Panel Status': [],
+                    f'Panel Label': [],
+                    f'Criterion': [],
+                    f'Panel Objective': [],
+                    f'Panel Frequency': [],
+                    f'Panel To Do': []
+                },
+                't2w': {
+                    f'T2W StratumId': [],
+                    f'T2W Status': [],
+                    f'T2W Label': [],
+                    f'Criterion': [],
+                    f'T2W Objective': [],
+                    f'T2W Frequency': [],
+                    f'T2W To Do': []
+                }
+            }
 
         for item in data:
             if self.source != 'Web':
@@ -66,20 +101,38 @@ class DataManagement(API):
                 output[f'{self.source} Objective'].append(item['Quota'])
                 output[f'{self.source} Frequency'].append(item['Frequence'])
                 output[f'{self.source} To Do'].append((item['Quota'] - item['Frequence']) if item['Quota'] > 0 else 0)
+
             elif self.source == 'Web':
-                output['Web StratumId'].append(item['StratumId'])
-                output['Web Status'].append(item['Status'])
-                output['Web Label'].append(item['Label'])
-                output['Criterion'].append(item['Criterion']
-                                           .replace(" AND STYPE>2", '')
-                                           .replace(" AND STYPE=3", '')
-                                           .replace(" AND STYPE=4", ''))
-                output['Web Objective'].append(item['Objective'])
-                output['Web Frequency'].append(item['Frequency'])
-                output['Web To Do'].append((item['Objective'] - item['Frequency']) if item['Objective'] > 0 else 0)
+
+                if "STYPE=3" in item['Criterion']:
+                    output['panel']['Panel StratumId'].append(item['StratumId'])
+                    output['panel']['Panel Status'].append(item['Status'])
+                    output['panel']['Panel Label'].append(item['Label'])
+                    output['panel']['Criterion'].append(item['Criterion']
+                                               .replace(" AND STYPE>2", '')
+                                               .replace(" AND STYPE=3", '')
+                                               .replace(" AND STYPE=4", ''))
+                    output['panel']['Panel Objective'].append(item['Objective'])
+                    output['panel']['Panel Frequency'].append(item['Frequency'])
+                    output['panel']['Panel To Do'].append((item['Objective'] - item['Frequency']) if item['Objective'] > 0 else 0)
+
+                elif "STYPE=4" in item['Criterion']:
+                    output['t2w']['T2W StratumId'].append(item['StratumId'])
+                    output['t2w']['T2W Status'].append(item['Status'])
+                    output['t2w']['T2W Label'].append(item['Label'])
+                    output['t2w']['Criterion'].append(item['Criterion']
+                                               .replace(" AND STYPE>2", '')
+                                               .replace(" AND STYPE=3", '')
+                                               .replace(" AND STYPE=4", ''))
+                    output['t2w']['T2W Objective'].append(item['Objective'])
+                    output['t2w']['T2W Frequency'].append(item['Frequency'])
+                    output['t2w']['T2W To Do'].append((item['Objective'] - item['Frequency']) if item['Objective'] > 0 else 0)
+
             else:
                 return pd.DataFrame()
-
+        if self.source == 'Web':
+            # print(json.dumps(output, indent=4))
+            return output['panel'], output['t2w']
         return output
 
     def get_data(self):
@@ -126,40 +179,69 @@ class DataManagement(API):
     def merge_data(self):
         df_com = self._com_data
         df_web = self._web_data
+        df_panel = self._panel_data
+        df_t2w = self._t2w_data
         df_ll = self._landline_data
         df_cell = self._cell_data
 
-        print("\033[93m\ncom\033[0m")
-        print(df_com.to_string())
-        print("\033[93m\nweb\033[0m")
-        print(df_web.to_string())
-        print("\033[93m\nlandline\033[0m")
-        print(df_ll.to_string())
-        print("\033[93m\ncell\033[0m")
-        print(df_cell.to_string())
+        # print("\033[93m\ncom\033[0m")
+        # print(df_com.to_string())
+        #
+        # print("\033[93m\nweb\033[0m")
+        # print(df_web.to_string())
+        #
+        # print("\033[93m\npanel\033[0m")
+        # print(df_panel.to_string())
+        #
+        # print("\033[93m\nt2w\033[0m")
+        # print(df_t2w.to_string())
+        #
+        # print("\033[93m\nlandline\033[0m")
+        # print(df_ll.to_string())
+        # print("\033[93m\ncell\033[0m")
+        # print(df_cell.to_string())
 
         # print(df_com.to_string())
         # print(df_web.to_string())
 
         try:
-            df_comweb = pd.merge(df_com, df_web, on=["Criterion"], how='left')
-            df_cwll = pd.merge(df_comweb, df_ll, on=["Criterion"], how='left')
-            df_cwlc = pd.merge(df_cwll, df_cell, on="Criterion", how='left')
+            df_com_panel_merge = pd.merge(df_com, df_panel, on=["Criterion"], how='left')
+            df_com_panel_tw2_merge = pd.merge(df_com_panel_merge, df_t2w, on=["Criterion"], how='left')
+            df_cptll_merge = pd.merge(df_com_panel_tw2_merge, df_ll, on=["Criterion"], how='left')
+            df_cptllc = pd.merge(df_cptll_merge, df_cell, on=["Criterion"], how='left')
+            # df_comweb = pd.merge(df_com, df_web, on=["Criterion"], how='left')
+            # df_cwll = pd.merge(df_comweb, df_ll, on=["Criterion"], how='left')
+            # df_cptllc = pd.merge(df_cwll, df_cell, on="Criterion", how='left')
         except Exception as err:
             print(traceback.format_exc())
             print(err)
 
-        df_cwlc.fillna(0, inplace=True)
+        df_cptllc.fillna(0, inplace=True)
 
-        df_cwlc['W%'] = np.round(df_cwlc['Web Frequency'] * 100 / df_cwlc['COM Frequency'], 2)
-        df_cwlc['L%'] = np.round(df_cwlc['LL Frequency'] * 100 / df_cwlc['COM Frequency'], 2)
-        df_cwlc['C%'] = np.round(df_cwlc['Cell Frequency'] * 100 / df_cwlc['COM Frequency'], 2)
-        df_cwlc['COM Frequency'] = df_cwlc['Web Frequency'] + df_cwlc['LL Frequency'] + df_cwlc['Cell Frequency']
-        df_cwlc['COM To Do'] = df_cwlc['COM Objective'] - df_cwlc['COM Frequency']
+        # Web Percent
+        df_cptllc['Web Frequency'] = df_cptllc['Panel Frequency'] + df_cptllc['T2W Frequency']
+
+        # COM Calculated Frequency
+        df_cptllc['COM Frequency'] = df_cptllc['Web Frequency'] + df_cptllc['LL Frequency'] + df_cptllc['Cell Frequency']
+        df_cptllc['COM To Do'] = df_cptllc['COM Objective'] - df_cptllc['COM Frequency']
+
+        df_cptllc['W%'] = np.round(df_cptllc['Web Frequency'] * 100 / df_cptllc['COM Frequency'], 2)
+        df_cptllc['P%'] = np.round(df_cptllc['Panel Frequency'] * 100 / df_cptllc['COM Frequency'], 2)
+        df_cptllc['T%'] = np.round(df_cptllc['T2W Frequency'] * 100 / df_cptllc['COM Frequency'], 2)
+        # df_cptllc['Phone%'] = np.round((df_cptllc['LL Frequency'] + df_cptllc['Cell Frequency']) * 100 / df_cptllc['COM Frequency'], 2)
+
+        #Phone %
+        df_cptllc['L%'] = np.round(df_cptllc['LL Frequency'] * 100 / df_cptllc['COM Frequency'], 2)
+        df_cptllc['C%'] = np.round(df_cptllc['Cell Frequency'] * 100 / df_cptllc['COM Frequency'], 2)
+        df_cptllc['Phone%'] = np.round((df_cptllc['LL Frequency'] + df_cptllc['Cell Frequency']) * 100 / df_cptllc['COM Frequency'], 2)
+
+
 
         self.reset()
 
-        return df_cwlc
+        # print(df_cptllc.to_string())
+
+        return df_cptllc
 
     @property
     def source(self):
@@ -172,6 +254,8 @@ class DataManagement(API):
     def reset(self):
         self._com_data = pd.DataFrame()
         self._web_data = pd.DataFrame()
+        self._panel_data = pd.DataFrame()
+        self._t2w_data = pd.DataFrame()
         self._landline_data = pd.DataFrame()
         self._cell_data = pd.DataFrame()
         self._source = None
